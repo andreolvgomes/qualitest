0312f32f56ee3de6b64cee01f33ee8b1b5071950
André Oliveira Gomes <inforoliver@gmail.com>
2026-05-25 12:58:17 -0300

    Excluir Suite

---
+++ QualiTest/Features/Suites/DeleteSuitesEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using FastEndpoints;
+using Infra.Repositories;
+
+namespace QualiTest.Features.Suites
+{
+    [HttpDelete("suites/{id:Guid}")]
+    public class DeleteSuitesEndpoint : EndpointWithoutRequest
+    {
+        private readonly IRepositoryBase<SuitesEntity> _repository;
+
+        public DeleteSuitesEndpoint(IRepositoryBase<SuitesEntity> repository)
+        {
+            _repository = repository;
+        }
+
+        public async override Task HandleAsync(CancellationToken ct)
+        {
+            await _repository.Delete(Route<Guid>("id"));
+            await Send.OkAsync();
+        }
+    }
+}
