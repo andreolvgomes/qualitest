9079d50ddfa86018eb9764662a3520c62c860b39
André Oliveira Gomes <inforoliver@gmail.com>
2026-05-27 13:02:06 -0300

    Salvando alterações feitas em Casos e Suites

---
+++ QualiTest/Features/Casos/UpdateCasosEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using FastEndpoints;
+using Infra.Repositories;
+
+namespace QualiTest.Features.Casos
+{
+    [HttpPut("casos/{id:Guid}")]
+    public class UpdateCasosEndpoint : Endpoint<CasosEntity>
+    {
+        private readonly ICasosRepository _casosRepository;
+
+        public UpdateCasosEndpoint(ICasosRepository casosRepository)
+        {
+            _casosRepository = casosRepository;
+        }
+
+        public async override Task HandleAsync(CasosEntity req, CancellationToken ct)
+        {
+            await _casosRepository.Update(req);
+            await Send.OkAsync();
+        }
+    }
+}
---
+++ QualiTest/Features/Suites/UpdateSuitesEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using FastEndpoints;
+using Infra.Repositories;
+
+namespace QualiTest.Features.Suites
+{
+    [HttpPut("suites/{id:Guid}")]
+    public class UpdateSuitesEndpoint : Endpoint<SuitesEntity>
+    {
+        private readonly IRepositoryBase<SuitesEntity> _repository;
+
+        public UpdateSuitesEndpoint(IRepositoryBase<SuitesEntity> repository)
+        {
+            _repository = repository;
+        }
+
+        public async override Task HandleAsync(SuitesEntity req, CancellationToken ct)
+        {
+            await _repository.Update(req);
+            await Send.OkAsync();
+        }
+    }
+}
